47ca1192e76d5e829bd74355141f30469255a4ac
Alona Kuznietsova <acvetochka@gmail.com>
2023-10-27 23:05:02 +0200

    for merge

--- models/user.js
+++ models/user.js
@@ -37,7 +37,7 @@ const userSchema = new Schema(
     },
     avatarURL: {
       type: String,
-      required: true,
+      // required: true,
     },
   },
   { versionKey: false, timestamps: true }
