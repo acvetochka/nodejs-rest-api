ec1635044483c2d1b3699addae5863f99be7e534
Alona Kuznietsova <acvetochka@gmail.com>
2024-09-14 22:15:37 +0200

    change register-controller

--- controllers/users/registerUser.js
+++ controllers/users/registerUser.js
@@ -3,14 +3,17 @@ const gravatar = require("gravatar");
 const { nanoid } = require("nanoid");
 
 const { User } = require("../../models/user");
-const { HttpError, sendEmail, verifyEmail } = require("../../helpers");
-const loginUser = require("./loginUser");
+// const { HttpError, sendEmail, verifyEmail } = require("../../helpers");
+const { HttpError } = require("../../helpers");
+// const loginUser = require("./loginUser");
 
 const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
   const hash = await bcrypt.hash(password, 10);
   const verificationToken = nanoid();
+
   const user = await User.findOne({ email });
+  console.log(user);
 
   if (user) {
     throw HttpError(409, "Email in use");
@@ -18,15 +21,26 @@ const registerUser = async (req, res) => {
 
   const avatarURL = gravatar.url(email);
 
-  const result = await User.create({ ...req.body, password: hash, verificationToken });
-  await sendEmail(verifyEmail(email, verificationToken));
+  const result = await User.create({
+    ...req.body,
+    password: hash,
+    verificationToken,
+  });
+  console.log("result: ", result);
 
-  const logUser = await loginUser(req.body)
-  const {token} = logUser;
-  
+  // await sendEmail(verifyEmail(email, verificationToken));
+
+  // const us = {
+  //   email, password
+  // }
+  // const logUser = await loginUser(us)
+  // console.log("logUser: ", logUser);
+
+  // const {token} = logUser;
+
   res.status(201).json({
     user: {
-      token,
+      // token,
       name,
       email,
       subscription: result.subscription,
